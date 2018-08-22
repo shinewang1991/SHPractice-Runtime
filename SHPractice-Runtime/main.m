c5a41ceff806c99741ff2c3487f20eea30febc0e
wangxiang <wangxiang@yixia.com>
2018-08-22 11:21:18 +0800

    Initial Commit

---
+++ SHPractice-Runtime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SHPractice-Runtime
+//
+//  Created by shine on 2018/8/22.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
