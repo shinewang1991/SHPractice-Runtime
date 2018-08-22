6bc01752bb2d1024de12762003daa96cc74b452b
wangxiang <wangxiang@yixia.com>
2018-08-22 18:14:05 +0800

    method swizzing improve

--- SHPractice-Runtime/Person+testB.m
+++ SHPractice-Runtime/Person+testB.m
@@ -11,22 +11,26 @@
 
 @implementation Person (testB)
 + (void)load{
-    Class class = self;
-    SEL originSelector = @selector(eat);
-    SEL newSelector = @selector(testEat);
-    Method originMethod = class_getInstanceMethod(class, originSelector);
-    Method newMethod = class_getInstanceMethod(class, newSelector);
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        SEL originSelector = @selector(eat);
+        SEL newSelector = @selector(testEat);
+        Method originMethod = class_getInstanceMethod(class, originSelector);
+        Method newMethod = class_getInstanceMethod(class, newSelector);
+        
+        BOOL didAddMethod = class_addMethod(class, originSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
+        //给当前class添加eat为名字testEat为实现的方法。
+        //YES: 以前不存在eat的实现,新添加的方法成功
+        //NO: 以前就存在eat的实现，所以新添加方法失败
+        if(didAddMethod){  //添加eat为名字testEat实现成功，然后需要交换testEat为名字的方法的实现为eat。 双双互换。
+            class_replaceMethod(class, newSelector, method_getImplementation(originMethod), method_getTypeEncoding(originMethod));
+        }
+        else{  //没添加成功，说明eat为名字的eat实现存在。 只需要交换eat和testEat两个的实现即可.
+            method_exchangeImplementations(originMethod, newMethod);
+        }
+    });
     
-    BOOL didAddMethod = class_addMethod(class, originSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
-    //给当前class添加eat为名字testEat为实现的方法。
-    //YES: 以前不存在eat的实现,新添加的方法成功
-    //NO: 以前就存在eat的实现，所以新添加方法失败
-    if(didAddMethod){  //添加eat为名字testEat实现成功，然后需要交换testEat为名字的方法的实现为eat。 双双互换。
-        class_replaceMethod(class, newSelector, method_getImplementation(originMethod), method_getTypeEncoding(originMethod));
-    }
-    else{  //没添加成功，说明eat为名字的eat实现存在。 只需要交换eat和testEat两个的实现即可.
-        method_exchangeImplementations(originMethod, newMethod);
-    }
 }
 
 + (void)initialize{
