6cfbcac7ab67119b3fbd832c39d1a875ad792b54
wangxiang <wangxiang@yixia.com>
2018-08-23 19:00:45 +0800

    objc_msgSend

---
+++ SHPractice-Runtime/MsgSend.h
@@ -0,0 +1,13 @@
+//
+//  MsgSend.h
+//  SHPractice-Runtime
+//
+//  Created by shine on 2018/8/23.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MsgSend : NSObject
+- (void)runDemo;
+@end
--- SHPractice-Runtime/main.m
+++ SHPractice-Runtime/main.m
@@ -8,12 +8,15 @@
 
 #import <Foundation/Foundation.h>
 #import "Son.h"
+#import "MsgSend.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSLog(@"Hello, World!");
-        Person *p = [[Person alloc] init];
-        [p eat];    //这里的eat方法已经被替换成testEat方法了。这就是method swizzing.
+//        NSLog(@"Hello, World!");
+//        Person *p = [[Person alloc] init];
+//        [p eat];    //这里的eat方法已经被替换成testEat方法了。这就是method swizzing.
+        MsgSend *msgSend = [MsgSend new];
+        [msgSend runDemo];
     }
     return 0;
 }
