7874b94d72055dae1b5322a81955c03234b526a6
wangxiang <wangxiang@yixia.com>
2018-08-27 11:42:38 +0800

    resolveInstanceMethod

--- SHPractice-Runtime/Person.m
+++ SHPractice-Runtime/Person.m
@@ -7,9 +7,26 @@
 //
 
 #import "Person.h"
+#import <objc/runtime.h>
+//#import <objc/message.h>
+
+void dynamicMethodIMP(id self, SEL _cmd){
+    //implementaion...
+    printf("dynamicMethodIMP \n");
+}
 
 @implementation Person
-- (void)eat{
-    NSLog(@"%s",__func__);
+
++ (BOOL)resolveClassMethod:(SEL)sel{
+    
+    return [super resolveClassMethod:sel];
+}
+
++ (BOOL)resolveInstanceMethod:(SEL)sel{
+    if(sel == @selector(eat)){
+        class_addMethod([self class], sel, (IMP)dynamicMethodIMP, "v@:");
+        return YES;
+    }
+    return [super resolveInstanceMethod:sel];
 }
 @end
--- SHPractice-Runtime/main.m
+++ SHPractice-Runtime/main.m
@@ -7,16 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Son.h"
-#import "MsgSend.h"
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 //        NSLog(@"Hello, World!");
-//        Person *p = [[Person alloc] init];
-//        [p eat];    //这里的eat方法已经被替换成testEat方法了。这就是method swizzing.
-        MsgSend *msgSend = [MsgSend new];
-        [msgSend runDemo];
+        Person *p = [[Person alloc] init];
+        [p eat];
     }
     return 0;
 }
